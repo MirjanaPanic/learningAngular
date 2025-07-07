67f68e7c0980a4db0ba092dd048cfe0b35c97300
MirjanaPanic <panicmirjanaa@gmail.com>
2025-07-07 18:21:48 +0200

    service created

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root', //<app-root> u html-u za izgradnju str.
-  standalone: true,
+  //standalone: true,
   imports: [HeaderComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
