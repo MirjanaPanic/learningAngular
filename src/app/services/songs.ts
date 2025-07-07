3e6424a05da107ea65a66f19746acd0d8c023cfb
MirjanaPanic <panicmirjanaa@gmail.com>
2025-07-07 18:38:59 +0200

    dependency injection

--- src/app/services/songs.ts
+++ src/app/services/songs.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+//koristi Dependency Injection mehanizam
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root', //bice dostupan servis svuda u aplikaciji
 })
 export class Songs {
-
-  constructor() { }
+  //ne kreiram prop, vec mi sam mehanizam DI injectuje instancu HttpClient :)
+  constructor(private httpClient: HttpClient) {}
 }
