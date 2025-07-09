a8ef3be6a8b79d63997d042e9ae6a415b44b2d47
MirjanaPanic <panicmirjanaa@gmail.com>
2025-07-09 21:07:49 +0200

    json-server REST API

--- src/app/components/songslist/songslist.ts
+++ src/app/components/songslist/songslist.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SongsService } from '../../services/songs';
+import { Song } from '../../models/song';
 
 @Component({
   selector: 'app-songslist',
@@ -8,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './songslist.css',
 })
 export class SongslistComponent {
-  songs: string[] = ['prva', 'druga', 'treca', 'cetvrta'];
+  songs: Song[] = [];
+
+  constructor(private songsService: SongsService) {} //instanca servisa
+
+  ngOnInit(): void {
+    this.songsService.getAll().subscribe((songs) => this.songs=songs);
+  }
 }
--- src/app/services/songs.ts
+++ src/app/services/songs.ts
@@ -1,12 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Song } from '../models/song';
+import { Observable } from 'rxjs';
 
 //koristi Dependency Injection mehanizam
 
 @Injectable({
   providedIn: 'root', //bice dostupan servis svuda u aplikaciji
 })
-export class Songs {
+export class SongsService {
   //ne kreiram prop, vec mi sam mehanizam DI injectuje instancu HttpClient :)
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {} //this.httpClient
+
+  getAll(): Observable<Song[]> {
+    return this.httpClient.get<Song[]>(environment.api + '/songs');
+  }
 }
